d7bf085cf0594abda497652265e612d4f2863b8a
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-09-25 17:19:27 +0200

    Add Favorites View Template

---
+++ SampleMiamUIKitIntegration/GeneralViews/FavoritesViewController.swift
@@ -0,0 +1,70 @@
+//
+//  FavoritesViewController.swift
+//  SampleMiamUIKitIntegration
+//
+//  Created by didi on 25/09/2023.
+//
+
+import UIKit
+import SwiftUI
+import MiamIOSFramework
+import MiamNeutraliOSFramework
+import miamCore
+
+/// This sets the Templates for the Favorites Overview
+public class MiamNeutralFavoritesParams: FavoritesViewParameters {
+    weak var navigationController: UINavigationController?
+    public init(navigationController: UINavigationController?) {
+        self.navigationController = navigationController
+    }
+    
+    public var recipeCard = MiamRecipeCard()
+    public var recipeCardLoading = MiamRecipeCardLoading()
+    @DefaultLoadingViewTemplate public var loading
+    @DefaultEmptyViewTemplate public var empty
+    
+    public lazy var showRecipes: (MiamIOSFramework.CatalogPackage) -> Void = {[weak self] _ in}
+    public lazy var noResultsRedirect: () -> Void = {[weak self] in}
+    public lazy var onRecipeTapped: (String) -> Void = { [weak self] recipe in
+        UserDefaults.standard.set(recipe, forKey: "miam_catalog_recipeId")
+        guard let strongSelf = self else { return }
+        strongSelf.navigationController?.pushViewController(MealPlannerRecipeDetailsViewController(), animated: true)
+    }
+}
+
+class FavoritesViewController: UIViewController {
+    deinit {
+        print("deinit: FavoritesViewController is being deallocated")
+    }
+    // Your SwiftUI View
+    var swiftUIView: FavoritesViewTemplate<
+        MiamNeutralFavoritesParams> {
+        return FavoritesViewTemplate.init(
+            params: MiamNeutralFavoritesParams(navigationController: self.navigationController)
+        )
+    }
+    // The hosting controller for your SwiftUI view
+    private var hostingController: UIHostingController<FavoritesViewTemplate<
+        MiamNeutralFavoritesParams>>?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Filters"
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Retour", style: .plain, target: nil, action: nil)
+        // Initialize the hosting controller with your SwiftUI view
+        hostingController = UIHostingController(rootView: swiftUIView)
+        guard let hostingController = hostingController, let hcView = hostingController.view
+        else { return }
+        // Since hostingController is optional, using guard to safely unwrap its view
+        hcView.translatesAutoresizingMaskIntoConstraints = false
+        addChild(hostingController)
+        view.addSubview(hcView)
+        NSLayoutConstraint.activate([
+            hcView.topAnchor.constraint(equalTo: view.topAnchor),
+            hcView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            hcView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hcView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        hostingController.didMove(toParent: self)
+    }
+}
