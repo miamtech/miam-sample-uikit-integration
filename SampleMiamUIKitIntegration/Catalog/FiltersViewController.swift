9adb5a5d1cc3d02c8825517d262a60b309dac492
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-10-17 10:50:26 +0200

    add filters code comments

--- SampleMiamUIKitIntegration/Catalog/FiltersViewController.swift
+++ SampleMiamUIKitIntegration/Catalog/FiltersViewController.swift
@@ -37,7 +37,8 @@ class FiltersViewController: UIViewController {
                 }, onClosed: { [weak self] in
                     guard let strongSelf = self else { return }
                     strongSelf.navigationController?.popViewController(animated: true)
-                }),
+                }
+            ),
             singletonFilterViewModel: singletonFilterViewModel
         )
     }
