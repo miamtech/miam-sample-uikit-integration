51145a558afe5c7c29cb60e9bf39b41558d3fd66
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-11-27 13:18:08 -0500

    add address & phone number

--- SampleMiamUIKitIntegration/GeneralViews/SelectStore/MyStoreViewController.swift
+++ SampleMiamUIKitIntegration/GeneralViews/SelectStore/MyStoreViewController.swift
@@ -13,35 +13,18 @@ class MyStoreViewController: UIViewController {
     
     let scrollView = UIScrollView()
     let imageView = UIImageView()
-    let storeNameLabel = UILabel()
-    let changeStoreButton = UIButton()
+    var changeStoreButton = UIButton()
     let contactLabel = UILabel()
-    let hoursLabel = UILabel()
+    var hoursLabel = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupScrollView()
         setupImageView()
-        //        setupStoreNameLabel()
-        //        setupChangeStoreButton()
-        //        setupContactLabel()
+                setupContactLabel()
         //        setupHoursLabel()
         
         self.view.backgroundColor = .white
-        // Create a label
-        let label = UILabel()
-        label.text = "My Store"
-        label.textAlignment = .center
-        
-        // Add the label to the view
-        view.addSubview(label)
-        
-        // Set constraints to center the label
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
     }
     
     func setupScrollView() {
@@ -129,25 +112,76 @@ class MyStoreViewController: UIViewController {
         config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)  // Padding around the button's content
         config.baseBackgroundColor = .white
         
-        let button = UIButton(configuration: config, primaryAction: nil)
-        button.setTitle("Changer de magasin", for: .normal)
-        button.setTitleColor(UIColor(Color.miamColor(.primary)), for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
-        button.layer.cornerRadius = 10
+        changeStoreButton = UIButton(configuration: config, primaryAction: nil)
+        changeStoreButton.setTitle("Changer de magasin", for: .normal)
+        changeStoreButton.setTitleColor(UIColor(Color.miamColor(.primary)), for: .normal)
+        changeStoreButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        changeStoreButton.layer.cornerRadius = 10
         
         // Shadow properties
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOffset = CGSize(width: 0, height: 2)
-        button.layer.shadowRadius = 3
-        button.layer.shadowOpacity = 0.3
+        changeStoreButton.layer.shadowColor = UIColor.black.cgColor
+        changeStoreButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        changeStoreButton.layer.shadowRadius = 3
+        changeStoreButton.layer.shadowOpacity = 0.3
 
-        button.translatesAutoresizingMaskIntoConstraints = false
-        imageView.addSubview(button)
+        changeStoreButton.translatesAutoresizingMaskIntoConstraints = false
+        imageView.addSubview(changeStoreButton)
         // Constraints for the button
         NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
-            button.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -20)
+            changeStoreButton.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
+            changeStoreButton.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -20)
             
+        ])
+    }
+    
+    func setupContactLabel() {
+        scrollView.addSubview(contactLabel)
+        NSLayoutConstraint.activate([
+            contactLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            contactLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0),
+            contactLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 0),
+            contactLabel.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        
+        // Create and add a label to the imageView
+        let titleLabel = UILabel()
+        titleLabel.text = "Contactez-nous"
+        titleLabel.font = UIFont.systemFont(ofSize: 26, weight: .bold)
+        titleLabel.textColor = .black
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        contactLabel.addSubview(titleLabel)
+        
+        let telephoneLabel = UILabel()
+        telephoneLabel.text = "Par téléphone : 03 03 03 03 03"
+        telephoneLabel.font = UIFont.systemFont(ofSize: 20)
+        telephoneLabel.textColor = .black
+        telephoneLabel.translatesAutoresizingMaskIntoConstraints = false
+        contactLabel.addSubview(telephoneLabel)
+        
+        let emailLabel = UILabel()
+        emailLabel.text = "Par mail : contact@miam.tech"
+        emailLabel.font = UIFont.systemFont(ofSize: 20)
+        emailLabel.textColor = .black
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        contactLabel.addSubview(emailLabel)
+        
+        // Create the stack view and add your views
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, telephoneLabel, emailLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.spacing = 12 // Adjust the spacing as needed
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        contactLabel.addSubview(stackView)
+        contactLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Constraints for the stack view
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: contactLabel.leadingAnchor, constant: 20), // Adjust constant as needed
+            stackView.topAnchor.constraint(equalTo: contactLabel.topAnchor, constant: 20), // Adjust constant as needed
+            stackView.trailingAnchor.constraint(lessThanOrEqualTo: contactLabel.trailingAnchor, constant: -20) // This constraint ensures the stackView doesn't exceed the width of contactLabel
         ])
     }
 }
