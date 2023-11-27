36d4b98d8cd035ac2980f9ffebc7d187ec32e546
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-11-27 14:01:50 -0500

    add hours of operation

--- SampleMiamUIKitIntegration/GeneralViews/SelectStore/MyStoreViewController.swift
+++ SampleMiamUIKitIntegration/GeneralViews/SelectStore/MyStoreViewController.swift
@@ -15,14 +15,15 @@ class MyStoreViewController: UIViewController {
     let imageView = UIImageView()
     var changeStoreButton = UIButton()
     let contactLabel = UILabel()
-    var hoursLabel = UIStackView()
+    let hoursLabel = UILabel()
+    let storeHoursViewController = StoreHoursViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupScrollView()
         setupImageView()
-                setupContactLabel()
-        //        setupHoursLabel()
+        setupContactLabel()
+        setupHoursLabel()
         
         self.view.backgroundColor = .white
     }
@@ -35,6 +36,31 @@ class MyStoreViewController: UIViewController {
             scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func setupHoursLabel() {
+        let hoursLabel = UILabel()
+        scrollView.addSubview(hoursLabel)
+        hoursLabel.text = "Horaires"
+        hoursLabel.font = UIFont.systemFont(ofSize: 26, weight: .bold)
+        hoursLabel.textColor = .black
+        hoursLabel.translatesAutoresizingMaskIntoConstraints = false
+        hoursLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hoursLabel.topAnchor.constraint(equalTo: contactLabel.bottomAnchor, constant: 10),
+            hoursLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+            hoursLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -80)
+        ])
+
+        scrollView.addSubview(storeHoursViewController)
+        let storeHoursHeight: CGFloat = 700
+        storeHoursViewController.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            storeHoursViewController.topAnchor.constraint(equalTo: hoursLabel.bottomAnchor, constant: 10),
+            storeHoursViewController.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            storeHoursViewController.heightAnchor.constraint(equalToConstant: storeHoursHeight), // fixed height for the imageView
+            storeHoursViewController.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor)
         ])
     }
     
@@ -136,10 +162,12 @@ class MyStoreViewController: UIViewController {
     
     func setupContactLabel() {
         scrollView.addSubview(contactLabel)
+        let contactLabelHeight: CGFloat = 130
         NSLayoutConstraint.activate([
             contactLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
             contactLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0),
             contactLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 0),
+            contactLabel.heightAnchor.constraint(equalToConstant: contactLabelHeight),
             contactLabel.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
         ])
         
@@ -185,3 +213,62 @@ class MyStoreViewController: UIViewController {
         ])
     }
 }
+
+class StoreHoursViewController: UIView, UITableViewDataSource {
+    
+    let tableView = UITableView(frame: .zero, style: .plain)
+    let daysOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+    let openingHours = "8:00 - 21:00" // This could be an array if different for each day
+    
+    override init(frame: CGRect) {
+            super.init(frame: frame)
+            addSubview(tableView) // Add tableView to the view hierarchy
+            tableView.dataSource = self
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "dayCell")
+            
+            tableView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                tableView.topAnchor.constraint(equalTo: topAnchor),
+                tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+                tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            ])
+            // Other setup...
+        }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return daysOfWeek.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dayCell", for: indexPath)
+
+        // Create a default content configuration for the cell
+        var content = cell.defaultContentConfiguration()
+        content.text = daysOfWeek[indexPath.row]
+        content.secondaryText = openingHours
+
+        // Customize the appearance for the "Vendredi" row
+        if indexPath.row == 4 {
+            content.textProperties.color = .systemBlue
+            content.textProperties.font = .boldSystemFont(ofSize: 18)
+            content.secondaryTextProperties.color = .systemBlue
+        } else {
+            content.textProperties.color = .black
+            content.textProperties.font = .systemFont(ofSize: 18)
+            content.secondaryTextProperties.color = .black
+        }
+
+        // Apply the configuration to the cell
+        cell.contentConfiguration = content
+
+        return cell
+    }
+
+}
+
