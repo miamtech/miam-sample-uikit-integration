16116c87def12c5cad9bbd8e120eb7fe761a56ae
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-11-28 12:53:19 -0500

    add product view to home page

--- SampleMiamUIKitIntegration/GeneralViews/HomeViewController.swift
+++ SampleMiamUIKitIntegration/GeneralViews/HomeViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class HomeViewController: UIViewController {
     
     let scrollView = UIScrollView()
+    let carouselLabel = UILabel()
+    let carousel = UIScrollView()
+    let firstProduct = ProductView(frame: .zero)
+    let secondProduct = ProductView()
     let imageView = UIImageView()
     let visualsLabel = UILabel()
     let firstVisual = UIImageView()
@@ -19,6 +23,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         setupScrollView()
         setupImageView()
+        setupCarousel()
         setupVisualsView()
         view.backgroundColor = .white
         // Do any additional setup after loading the view.
@@ -34,6 +39,55 @@ class HomeViewController: UIViewController {
             scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+    
+    func setupCarousel() {
+        scrollView.addSubview(carouselLabel)
+        carouselLabel.text = "Nos produits du moment"
+        carouselLabel.font = UIFont.systemFont(ofSize: 26, weight: .bold)
+        carouselLabel.textColor = .black
+        carouselLabel.translatesAutoresizingMaskIntoConstraints = false
+       
+        NSLayoutConstraint.activate([
+            carouselLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            carouselLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+            carouselLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
+        ])
+        view.addSubview(carousel)
+        let carouselHeight: CGFloat = 250
+        carousel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            carousel.topAnchor.constraint(equalTo: carouselLabel.bottomAnchor, constant: 10),
+            carousel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            carousel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            carousel.heightAnchor.constraint(equalToConstant: carouselHeight),
+        ])
+        
+        
+        if let firstProductFromBasket = PretendBasket.shared.items.first {
+            firstProduct.configure(with: firstProductFromBasket)
+            carousel.addSubview(firstProduct)
+            firstProduct.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                firstProduct.topAnchor.constraint(equalTo: carousel.topAnchor, constant: 10),
+                firstProduct.bottomAnchor.constraint(equalTo: carousel.bottomAnchor),
+                firstProduct.widthAnchor.constraint(equalToConstant: CGFloat(300)),
+                firstProduct.leadingAnchor.constraint(equalTo: carousel.leadingAnchor),
+            ])
+        }
+        
+        if let secondProductFromBasket = PretendBasket.shared.items.last {
+            secondProduct.configure(with: secondProductFromBasket)
+            carousel.addSubview(secondProduct)
+            secondProduct.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                secondProduct.topAnchor.constraint(equalTo: carousel.topAnchor, constant: 10),
+                secondProduct.bottomAnchor.constraint(equalTo: carousel.bottomAnchor),
+                secondProduct.widthAnchor.constraint(equalToConstant: CGFloat(300)),
+                secondProduct.leadingAnchor.constraint(equalTo: firstProduct.trailingAnchor, constant: 10),
+                secondProduct.trailingAnchor.constraint(equalTo: carousel.trailingAnchor, constant: -30),
+            ])
+        }
+    }
 
     func setupImageView() {
         scrollView.addSubview(imageView)
@@ -41,11 +95,9 @@ class HomeViewController: UIViewController {
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         imageView.isUserInteractionEnabled = true
-        
         imageView.image = UIImage(named: "Hero_Slider")
         
         let imageHeight: CGFloat = 250
-        
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: scrollView.topAnchor), // top of the scrollView
             imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0), // leading of the scrollView
@@ -62,7 +114,7 @@ class HomeViewController: UIViewController {
         visualsLabel.textColor = .black
         visualsLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            visualsLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            visualsLabel.topAnchor.constraint(equalTo: carousel.bottomAnchor, constant: 10),
             visualsLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
             visualsLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20)
         ])
@@ -79,7 +131,6 @@ class HomeViewController: UIViewController {
             firstVisual.topAnchor.constraint(equalTo: visualsLabel.bottomAnchor, constant: 30), // top of the scrollView
             firstVisual.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10), // leading of the scrollView
             firstVisual.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10), // trailing of the scrollView
-//            firstVisual.heightAnchor.constraint(equalToConstant: visualHeight), // fixed height for the imageView
             firstVisual.widthAnchor.constraint(equalToConstant: visualWidth), // fixed height for the imageView
             firstVisual.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
         ])
@@ -94,7 +145,6 @@ class HomeViewController: UIViewController {
             secondVisual.topAnchor.constraint(equalTo: firstVisual.bottomAnchor, constant: 10), // top of the scrollView
             secondVisual.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10), // leading of the scrollView
             secondVisual.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10), // trailing of the scrollView
-//            secondVisual.heightAnchor.constraint(equalToConstant: visualHeight), // fixed height for the imageView
             secondVisual.widthAnchor.constraint(equalToConstant: visualWidth),
             secondVisual.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
             secondVisual.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor)
